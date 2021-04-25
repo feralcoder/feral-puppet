46a0a506aa596aaab725d0c17ca4cc8002a7524f
feralcoder <feralcoder@gmail.com>
2021-04-25 20:52:51 +0000

    puppetmaster module

---
+++ site-modules/feralcoder_puppetmaster/manifests/init.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include feralcoder_puppetmaster
+class feralcoder_puppetmaster {
+}
