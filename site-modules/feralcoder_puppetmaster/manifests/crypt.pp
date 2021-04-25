9301290fc5067f8e16b6689dbfae0c61135ab372
feralcoder <feralcoder@gmail.com>
2021-04-25 21:44:41 +0000

    place crypt/decrypt secret for environment from puppetmaster's own facter file

--- site-modules/feralcoder_puppetmaster/manifests/crypt.pp
+++ site-modules/feralcoder_puppetmaster/manifests/crypt.pp
@@ -5,4 +5,15 @@
 # @example
 #   include feralcoder_puppetmaster::crypt
 class feralcoder_puppetmaster::crypt {
+  file { '/etc/puppetlabs/code/environments/production/site-modules/feralcoder_common/files/':
+    ensure => directory,
+    owner => 'puppet',
+    group => 'wheel',
+    mode => '0750'
+  }
+  file { '/etc/puppetlabs/code/environments/production/site-modules/feralcoder_common/files/facts_decrypt.txt':
+    source => [ '/etc/facter/facts.d/decrypt.txt' ],
+    mode => '0600',
+    owner => 'puppet'
+  }
 }
