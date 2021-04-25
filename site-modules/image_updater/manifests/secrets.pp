4876a5e0b43e5db6f091c62afeec5ef75c731606
feralcoder <feralcoder@gmail.com>
2021-04-25 20:53:28 +0000

    image_updater module

---
+++ site-modules/image_updater/manifests/secrets.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include image_updater::secrets
+class image_updater::secrets {
+}
---
+++ site-modules/image_updater/manifests/ssh.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include image_updater::ssh
+class image_updater::ssh {
+}
