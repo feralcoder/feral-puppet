1717d9d36d545df0de881576d32fe18b0f05bd3e
feralcoder <feralcoder@gmail.com>
2021-04-26 17:06:46 +0000

    create status directory and end-run file

--- site-modules/image_updater/manifests/reboot.pp
+++ site-modules/image_updater/manifests/reboot.pp
@@ -4,7 +4,15 @@
 #
 # @example
 #   include image_updater::reboot
-class image_updater::reboot {
+class image_updater::reboot: {
+  file { '/etc/puppet_runs':
+    ensure => 'directory',
+    mode => '0755'
+  }
+  file { '/etc/puppet_runs/image_updater_finished':
+    ensure => 'file',
+    mode => '0644'
+  }
   reboot { 'after':
     subscribe       => Exec['dnf update'],
   }
