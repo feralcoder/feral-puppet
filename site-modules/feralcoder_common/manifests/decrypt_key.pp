5c92a662f9b9bd46de6d859d10bfc9480a3091fa
feralcoder <feralcoder@gmail.com>
2021-04-26 06:37:46 +0000

    set up facter directory; reload puppet after facter changes (refacter)

--- site-modules/feralcoder_common/manifests/decrypt_key.pp
+++ site-modules/feralcoder_common/manifests/decrypt_key.pp
@@ -7,22 +7,28 @@
 
 class feralcoder_common::decrypt_key {
 
-  file { [ '/etc/facter/', '/etc/facter/facts.d/' ]:
-    ensure => directory,
-    owner => 'root',
-    group => 'wheel',
-    mode => '0750'
+  # Using exec instead of file resource to break dependency cycle
+  exec { 'create parent directories for facter...':
+    command => "/usr/bin/mkdir -p /etc/facter/facts.d/"
   }
+
+#  file { [ '/etc/facter/', '/etc/facter/facts.d/' ]:
+#    ensure => directory,
+#    owner => 'root',
+#    group => 'wheel',
+#    mode => '0750'
+#  }
 
   file { '/etc/facter/facts.d/decrypt.txt':
     ensure => file,
     source => [ "puppet:///modules/feralcoder_common/facts_decrypt.txt" ],
     mode => "0600"
   }
+  ~> refacter { 'reload after decrypt key placed': patterns => [ "^fc_puppet_decrypt_key" ] }
 
-  ~> abort { 'Restart Puppet Agent With New Decrypt Secret':
-    message => 'Restart Puppet Agent With New Decrypt Secret'
-  }
+#  ~> abort { 'Restart Puppet Agent With New Decrypt Secret':
+#    message => 'Restart Puppet Agent With New Decrypt Secret'
+#  }
 #  ~> fail { 'Restart Puppet Agent With New Decrypt Secret': }
   file { '/tmp/test.txt':
     ensure => file,
