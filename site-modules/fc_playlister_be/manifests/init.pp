e3cf33f987d3cfcafba7134adb6bc33d4a47f2ac
Cliff McIntire <feralcoder@gmail.com>
2021-05-04 03:39:37 +0000

    most states represented and implemented or skeletonized

--- site-modules/fc_playlister_be/manifests/init.pp
+++ site-modules/fc_playlister_be/manifests/init.pp
@@ -5,24 +5,188 @@
 # @example
 #   include fc_playlister_be
 class fc_playlister_be {
+  exec { 'create fc_playlister_be state directory':
+    command => "/usr/bin/mkdir -p /etc/puppetlabs/fc_playlister_be_state"
+  }
 
-  if $playlister_cassandra_delayed_install == false {
-    class { 'fc_playlister_be::cassandra': }
-    file { '/tmp/cassandra.txt':
-      content => "$cassandra"
+  # INITIAL BUILD
+  if $fc_playlister_be_state_fact == 'initial_build' {
+    if $fc_playlister_be_state_manifest == 'initial_build' {
+      exec { 'clear fc_playlister_be state before change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/initial_build_started':
+        ensure => file,
+      }
+      ~> class { 'fc_playlister_be::cassandra': }
+      ~> exec { 'clear fc_playlister_be state after change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/initial_build_finished':
+        ensure => file,
+        require => Anchor['fc_playlister_be::cassandra::end'],
+      }
+    } else {
+      file { '/tmp/fc_playlister_be_state.txt':
+        content => "Current state is inconsistent"
+      }
     }
-    file { '/tmp/cassandra_package_name.txt':
-      content => "$cassandra::package_name"
+  }
+
+
+
+  # STARTING
+  if $fc_playlister_be_state_fact == 'starting' {
+    if $fc_playlister_be_state_manifest == 'starting' {
+      exec { 'clear fc_playlister_be state before change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/starting_started':
+        ensure => file,
+      }
+      ~> class { 'fc_playlister_be::cassandra_start': }
+      ~> exec { 'clear fc_playlister_be state after change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/starting_finished':
+        ensure => file,
+        require => Anchor['fc_playlister_be::cassandra_start::end'],
+      }
+    } else {
+      file { '/tmp/fc_playlister_be_state.txt':
+        content => "Current state is inconsistent"
+      }
     }
-    file { '/tmp/cassandra_install':
-      content => "true"
+  }
+
+
+
+  # LOAD_SCHEMA
+  if $fc_playlister_be_state_fact == 'load_schema' {
+    if $fc_playlister_be_state_manifest == 'load_schema' {
+      exec { 'clear fc_playlister_be state before change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/load_schema_started':
+        ensure => file,
+      }
+      ~> class { 'fc_playlister_be::cassandra': }
+      ~> class { 'fc_playlister_be::cassandra_schema': }
+      ~> exec { 'clear fc_playlister_be state after change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/load_schema_finished':
+        ensure => file,
+        require => Anchor['fc_playlister_be::cassandra::end', 'fc_playlister_be::cassandra_schema::end'],
+      }
+    } else {
+      file { '/tmp/fc_playlister_be_state.txt':
+        content => "Current state is inconsistent"
+      }
     }
-  } else {
-    file { '/tmp/cassandra_test':
-      content => "delayed"
+  }
+
+
+
+  # STARTED
+  if $fc_playlister_be_state_fact == 'started' {
+    if $fc_playlister_be_state_manifest == 'started' {
+      exec { 'clear fc_playlister_be state before change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/started_started':
+        ensure => file,
+      }
+      ~> class { 'fc_playlister_be::cassandra_started': }
+      ~> exec { 'clear fc_playlister_be state after change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/started_finished':
+        ensure => file,
+        require => Anchor['fc_playlister_be::cassandra_started::end'],
+      }
+    } else {
+      file { '/tmp/fc_playlister_be_state.txt':
+        content => "Current state is inconsistent"
+      }
     }
-    file { '/tmp/cassandra_install':
-      content => "delayed"
+  }
+
+
+
+  # NOMINAL
+  if $fc_playlister_be_state_fact == 'nominal' {
+    if $fc_playlister_be_state_manifest == 'nominal' {
+      exec { 'clear fc_playlister_be state before change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/nominal_started':
+        ensure => file,
+      }
+      ~> class { 'fc_playlister_be::cassandra_started': }
+      ~> exec { 'clear fc_playlister_be state after change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/nominal_finished':
+        ensure => file,
+        require => Anchor['fc_playlister_be::cassandra_started::end'],
+      }
+    } else {
+      file { '/tmp/fc_playlister_be_state.txt':
+        content => "Current state is inconsistent"
+      }
+    }
+  }
+
+
+
+  # STOPPING
+  if $fc_playlister_be_state_fact == 'stopping' {
+    if $fc_playlister_be_state_manifest == 'stopping' {
+      exec { 'clear fc_playlister_be state before change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/stopping_started':
+        ensure => file,
+      }
+      ~> class { 'fc_playlister_be::cassandra_stop': }
+      ~> exec { 'clear fc_playlister_be state after change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/stopping_finished':
+        ensure => file,
+        require => Anchor['fc_playlister_be::cassandra_stop::end'],
+      }
+    } else {
+      file { '/tmp/fc_playlister_be_state.txt':
+        content => "Current state is inconsistent"
+      }
+    }
+  }
+
+
+
+  # STOPPED
+  if $fc_playlister_be_state_fact == 'stopped' {
+    if $fc_playlister_be_state_manifest == 'stopped' {
+      exec { 'clear fc_playlister_be state before change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/stopped_started':
+        ensure => file,
+      }
+      ~> class { 'fc_playlister_be::cassandra_stopped': }
+      ~> exec { 'clear fc_playlister_be state after change':
+        command => "/usr/bin/rm -f /etc/puppetlabs/fc_playlister_be_state/*",
+      }
+      ~> file { '/etc/puppetlabs/fc_playlister_be_state/stopped_finished':
+        ensure => file,
+        require => Anchor['fc_playlister_be::cassandra_stopped::end'],
+      }
+    } else {
+      file { '/tmp/fc_playlister_be_state.txt':
+        content => "Current state is inconsistent"
+      }
     }
   }
 }
