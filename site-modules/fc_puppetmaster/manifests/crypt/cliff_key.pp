3c502f30dbaeeb67fb828280460275e19fb42df2
Cliff McIntire <feralcoder@gmail.com>
2021-05-05 21:28:25 +0000

    no quotes needed on facter.txt file

--- site-modules/fc_puppetmaster/manifests/crypt/cliff_key.pp
+++ site-modules/fc_puppetmaster/manifests/crypt/cliff_key.pp
@@ -7,6 +7,6 @@
 class fc_puppetmaster::crypt::cliff_key {
   $cliff_public_key_file='/etc/puppetlabs/code/environments/production/site-modules/fc_common/files/facts_cliff_key.txt'
   exec { 'set up cliff public key fact file':
-    command => "/usr/bin/cat /home/cliff/.ssh/pubkeys/id_rsa.pub | /usr/bin/sed -e 's/\(.*\)/fc_common_cliff_key=\"\1\"/g' > $cliff_public_key_file",
+    command => "/usr/bin/cat /home/cliff/.ssh/pubkeys/id_rsa.pub | /usr/bin/sed -e 's/\(.*\)/fc_common_cliff_key=\1/g' > $cliff_public_key_file",
   }
 }
